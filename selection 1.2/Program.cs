c087151ca2932a7523283f35f4a79fba7243bc8e
Becauseraisins <79813812+Becauseraisins@users.noreply.github.com>
2021-03-18 13:48:15 +1100

    Add files via upload
    yeah i know the names of all the items are hello_world I'm not going to bother changing it all back

---
+++ selection 1.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter your age in years");
+            string age = Console.ReadLine();
+            int ageNum = int.Parse(age);
+        
+            Console.WriteLine("please enter your gender M/F");
+            string gender = Console.ReadLine();
+           
+             
+            if(ageNum>17){
+                //true path
+                if(gender=="M"){
+                Console.WriteLine("Male adult");
+                }   
+
+                if(gender=="F"){
+                Console.WriteLine("Female Adult");
+                }
+                }
+            
+            
+            else{
+                //false path
+                if(gender=="M"){
+                Console.WriteLine("Male is not an Adult");
+                }
+
+                if(gender=="F"){
+                Console.WriteLine("Female is not an Adult");
+                }
+                
+               }
+          }
+    }
+}
+ 
